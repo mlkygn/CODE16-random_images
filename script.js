129228759d9ae49ff4094fd6204e680041ba1833
Melike Y <84517003+mlkygn@users.noreply.github.com>
2023-12-29 09:40:23 +0300

    initial

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.getElementById("imagesContainer");
+const getImages = () => {
+  for (let i = 0; i < 15; i++) {
+    container.innerHTML += `
+    <div class="card my-3"><img class="w-100" onload="this.parentElement.style.opacity = 1" src="https://source.unsplash.com/random/500x${
+      Math.floor(Math.random() * 301) + 500
+    }?sig=${Math.random()}"></div>
+   `;
+  }
+};
+
+getImages();
+
+document.querySelectorAll("img").forEach(function (elem) {
+  elem.addEventListener("click", function (e) {
+    document.getElementById("imagepreview").setAttribute("src", e.currentTarget.getAttribute("src")); // here asign the image to the modal when the user click the enlarge link
+  });
+});
